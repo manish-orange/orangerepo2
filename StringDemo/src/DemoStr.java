cdae02e6bf773dac0ff44ec96518273c4f4b369b
Manish <mkskrsri@gmail.com>
2019-10-31 15:13:49 +0530

    abhi abhi commited

---
+++ StringDemo/src/DemoStr.java
@@ -0,0 +1,18 @@
+
+public class DemoStr {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+		
+		String a = "ABC- 99";
+		int b = a.indexOf(" ");
+		System.out.println(b);
+        String c = a.replace(" ","");
+		System.out.println(c);
+
+		
+		
+	}
+
+}
